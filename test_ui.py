3149e2bdf13bb52740c273b00f86b2116a5e9835
kabird1 <148470042+kabird1@users.noreply.github.com>
2024-03-26 01:03:22 -0400

    Update test_ui.py

--- test_ui.py
+++ test_ui.py
@@ -30,11 +30,11 @@ if 'chunk_vector_store' not in st.session_state:
 
 st.session_state.vendor_name=st.chat_input(placeholder='Enter the name of a vendor:')
 if st.session_state.vendor_name!=None:
-    st.session_state.docs = st.session_state.full_doc_vector_store.similarity_search(query=st.session_state.vendor_name,k=2000, score_threshold=0.83)
+    st.session_state.docs = st.session_state.full_doc_vector_store.similarity_search_with_relevance_scores(query=st.session_state.vendor_name,k=2000, score_threshold=0.83)
     
 if st.session_state.docs!=None:
     st.write('The following documents are related to this vendor:')
     for doc in st.session_state.docs:
-        st.write('- '+doc.metadata['filename'])
+        st.write('- '+doc[0].metadata['filename'])
 
     
